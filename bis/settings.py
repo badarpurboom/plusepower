ac4352a0725db302f37d83aac283deace0e820d6
badarpurboom <73841497+badarpurboom@users.noreply.github.com>
2021-06-17 17:55:54 +0530

    6th

--- bis/settings.py
+++ bis/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,10 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-import os
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
