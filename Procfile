5f1f395949d6331bccc77a9d33e1976918230ec3
badarpurboom <73841497+badarpurboom@users.noreply.github.com>
2021-06-17 17:17:34 +0530

    hi
    2nd

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bis.wsgi --log-file -
--- bis/bis/settings.py
+++ bis/bis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--tj#2w3qc5_f7zn=l-+9=hb(d(i8z#kadk@p85xskslq-vnim2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['powerpluse.herokuapp.com','127.0.0.1']
 
 
 # Application definition
