4f151bc9430d7feac3c4b76ee7563fb74ac77dc1
badarpurboom <73841497+badarpurboom@users.noreply.github.com>
2021-06-24 14:21:53 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect, render
 from . models import *
 # Create your views here.
 
+@csrf_exempt
 def home(request):
     if request.method=='POST':
         name=request.POST['name']
